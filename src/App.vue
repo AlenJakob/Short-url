b8df3d3318cc3b1e435db6956d55f6736c9f9171
jundymek <jundymek@gmail.com>
2021-04-25 13:15:03 +0200

    feat: Statistics title and paragraph basic

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Statistics />
     <h1>Hello</h1>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Statistics from "./components/statistics/Statistics.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld,
+    Statistics,
   },
 });
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,5 +26,6 @@ export default Vue.extend({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-family: "Poppins", sans-serif;
 }
 </style>
---
+++ src/components/statistics/Statistics.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+    <header class="header">
+      <h3 class="title">Advanced Statistics</h3>
+      <p class="paragraph">
+        Track how your links are performing across the web with our advanced statistics dashboard.
+      </p>
+    </header>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  name: "Statictics",
+});
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.title {
+  font-size: 30px;
+}
+
+.paragraph {
+  color: hsl(257, 7%, 63%);
+  width: 60%;
+  margin: 0 auto;
+}
+</style>

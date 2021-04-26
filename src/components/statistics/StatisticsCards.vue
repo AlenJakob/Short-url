a444ce9ce8c18b47e50f16c92e4b1805287c2877
jundymek <jundymek@gmail.com>
2021-04-26 23:13:42 +0200

    refactor: StatisticCards v-for

--- src/components/statistics/StatisticsCards.vue
+++ src/components/statistics/StatisticsCards.vue
@@ -1,36 +1,48 @@
 <template>
   <div class="cards-wrapper">
     <StatisticsCard
-      title="Brand Recognition"
-      message="Boost your brand recognition with each click. Generic links don’t 
-    mean a thing. Branded links help instil confidence in your content."
-      icon="icon-brand-recognition.svg"
-    />
-    <StatisticsCard
-      title="Detailed Records"
-      message="Gain insights into who is clicking your links. Knowing when and where 
-  people engage with your content helps inform better decisions."
-      icon="icon-detailed-records.svg"
-      class="second-card"
-    />
-    <StatisticsCard
-      title="Fully Customizable"
-      message="Improve brand awareness and content discoverability through customizable 
-  links, supercharging audience engagement."
-      icon="icon-fully-customizable.svg"
-      class="third-card"
+      v-for="(card, index) in cards"
+      v-bind:title="card.title"
+      v-bind:message="card.message"
+      v-bind:icon="card.icon"
+      v-bind:class="card.class"
+      v-bind:key="index"
     />
   </div>
 </template>
 
 <script>
-// import Vue from "vue";
 import { defineComponent } from "vue";
 import StatisticsCard from "./StatisticsCard";
 export default defineComponent({
   name: "Statictics",
   components: {
     StatisticsCard,
+  },
+  data() {
+    const cards = [
+      {
+        title: "Brand Recognition",
+        message:
+          "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
+        icon: "icon-brand-recognition.svg",
+      },
+      {
+        title: "Detailed Records",
+        message:
+          "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+        icon: "icon-detailed-records.svg",
+        class: "second-card",
+      },
+      {
+        title: "Fully Customizable",
+        message:
+          "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
+        icon: "icon-fully-customizable.svg",
+        class: "third-card",
+      },
+    ];
+    return { cards };
   },
 });
 </script>

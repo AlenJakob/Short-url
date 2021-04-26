a298e6c4ada2f3abba47d1e9e1a08c5d2f1570b2
abioveprincipium <alicjajagodaa@gmail.com>
2021-04-26 21:50:51 +0200

    style for navigation desktop size

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,13 +1,17 @@
 <template>
 <div class="nav">
-  <img alt="Site logo" src="../assets/logo.svg" />
+  <img alt="Site logo" src="../assets/logo.svg" width="121" height="33" />
     <ul>
       <li><a href="/">Features</a></li>
-      <li>Pricing</li>
-      <li>Resources</li>
+      <li><a href="/">Pricing</a></li>
+      <li><a href="/">Resources</a></li>
     </ul>
-    <button> Login </button>
-    <button> Sign Up </button>
+    <div class="btn">
+      <ul>
+        <li><a href="/">Login</a></li>
+      </ul>
+      <button> Sign Up </button>
+    </div>
   </div>
 </template>
 
@@ -17,14 +21,64 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600;900&display=swap');
+  font-family: Poppins, sans-serif;
+  font-weight: 700;
+  font-size: 18px;
+  color: hsl(0, 0%, 75%);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: left;
   color: #2c3e50;
   margin-top: 20px;
-  ul a{
-  text-decoration: none;
+  .nav{
+      display: flex;
+      flex-direction: row;
+      padding: 30px 100px;
+    img{
+      padding-top: 15px;
+    }
+    .btn{
+      margin: 0px 0px 0px auto;
+      display: flex;
+      flex-direction: row;
+      button:hover{
+          background-color: hsl(172, 75%, 73%);
+        }
+      button{
+        margin-top:10px;
+        font-family: Poppins;
+        justify-content: center;
+        font-size: 16px;
+        margin-left: 15px;
+        color: #fff;
+        padding: 0px;
+        height: 35px;
+        width: 100px;
+        border: 0;
+        border-radius: 25px;
+        background-color: hsl(180, 66%, 49%);
+        outline: none;
+        cursor: pointer;
+        
+      }
+    }
+    ul {
+      display: flex;
+      flex-direction: row;
+      flex-wrap:wrap;
+      list-style-type: none;
+      li{
+        margin: 0px 15px 0px 10px;
+        :hover{
+          color:  hsl(0, 0%, 9%);
+        }
+      }
+      a{
+        text-decoration: none;
+        color: hsl(0, 0%, 75%);
+      }
+    }
   }
 }
 

52fb2618df304d3022146b2b3ed3699b7706abbf
jundymek <jundymek@gmail.com>
2021-04-29 18:28:54 +0200

    refactor: Links component

---
+++ src/components/SearchBar/Links.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="links">
+    <div class="link" v-for="link in props.links" :key="link.id">
+      <div class="regular">{{ link.regularLink }}</div>
+      <div class="action">
+        <div class="shorter">{{ link.shorterLink }}</div>
+        <button
+          @click="btnStatus(link.id, link.shorterLink)"
+          class="btn btn-copy"
+          :class="link.isActive ? 'isActive' : ''"
+        >
+          {{ link.isActive ? "Copied" : "Copy" }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  props: ["links"],
+  setup(props) {
+    console.log(props);
+    return { props };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.links {
+  margin: 15px 0;
+  font-size: 18px;
+  display: flex;
+  flex-direction: column;
+
+  & .action {
+    display: flex;
+    align-items: center;
+    & .shorter {
+      margin-right: 10px;
+    }
+    & .btn-copy {
+      padding: 10px 20px;
+    }
+  }
+  .link {
+    box-sizing: border-box;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #fff;
+    margin-bottom: 15px;
+    padding: 10px 20px;
+    border-radius: 4px;
+    color: hsl(180, 66%, 49%);
+    & .btn {
+      font-weight: bold;
+    }
+    & .regular {
+      color: hsl(257, 27%, 26%) !important;
+    }
+  }
+}
+.btn,
+.input {
+  cursor: pointer;
+  border-radius: 8px;
+  margin: 0 8px;
+}
+.btn {
+  background: hsl(180, 66%, 49%);
+  color: #fff;
+  border: none;
+  &:hover {
+    background: #9be3e2;
+  }
+}
+</style>
--- src/components/SearchBar/SearchBar.vue
+++ src/components/SearchBar/SearchBar.vue
@@ -6,21 +6,7 @@
         <!-- v-model on custom component  -->
         <Input :shorten="shorten" />
       </div>
-      <div class="links">
-        <div class="link" v-for="link in links" :key="link.id">
-          <div class="regular">{{ link.regularLink }}</div>
-          <div class="action">
-            <div class="shorter">{{ link.shorterLink }}</div>
-            <button
-              @click="btnStatus(link.id, link.shorterLink)"
-              class="btn btn-copy"
-              :class="link.isActive ? 'isActive' : ''"
-            >
-              {{ link.isActive ? "Copied" : "Copy" }}
-            </button>
-          </div>
-        </div>
-      </div>
+      <Links :links="links" />
       <h2>{{ copied }}</h2>
     </div>
   </div>
@@ -30,9 +16,11 @@
 import { ref, defineComponent } from "vue";
 import { uuid, validateInput, parseLink } from "./helpers.js";
 import Input from "./Input";
+import Links from "./Links";
 export default defineComponent({
   components: {
     Input,
+    Links,
   },
   setup() {
     const input = ref("");
@@ -125,54 +113,5 @@ export default defineComponent({
 
 .btn-action {
   font-weight: bold;
-}
-.links {
-  margin: 15px 0;
-  font-size: 18px;
-  display: flex;
-  flex-direction: column;
-
-  & .action {
-    display: flex;
-    align-items: center;
-    & .shorter {
-      margin-right: 10px;
-    }
-    & .btn-copy {
-      padding: 10px 20px;
-    }
-  }
-  .link {
-    box-sizing: border-box;
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background: #fff;
-    margin-bottom: 15px;
-    padding: 10px 20px;
-    border-radius: 4px;
-    color: hsl(180, 66%, 49%);
-    & .btn {
-      font-weight: bold;
-    }
-    & .regular {
-      color: hsl(257, 27%, 26%) !important;
-    }
-  }
-}
-.btn,
-.input {
-  cursor: pointer;
-  border-radius: 8px;
-  margin: 0 8px;
-}
-.btn {
-  background: hsl(180, 66%, 49%);
-  color: #fff;
-  border: none;
-  &:hover {
-    background: #9be3e2;
-  }
 }
 </style>
